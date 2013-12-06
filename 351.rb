cb5a2a6706a0944f49e5313ca13a9898789d2a63
Daniel Lu <dandydanny@gmail.com>
2013-12-05 22:10:03 -0600

    Create 351.rb

---
+++ 351.rb
@@ -0,0 +1,21 @@
+# Which step(s) do you gravitate to when solving a problem?
+
+# 	Observe
+
+# Which step(s) do you tend to skip when solving a problem?
+
+# 	Decide
+
+# What effects occur when you skip a step or spend too long in one step?
+	
+# 	If I spend too long during the decision step, there won't be too much time left to act
+
+# Select 1 of these coding feedback loops and describe how you observe, orient, decide, and act when in them.
+
+# 	During observation phase, I gather all available information pertaining to a problem at hand.
+
+# Identify at least 1 additional feedback loop in your daily life. Describe how you OODA it.
+
+# 	Deciding to learn web development in Chicago was the result of information gathering (scoping out the industry), make sense
+# 	of this information, deciding whether it would be beneficial, and finally, acting on the decision (to devote time and money
+# 		in making this happen).
